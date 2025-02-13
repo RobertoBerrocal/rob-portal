824f83f46b727ddf0b82320f730ef4705cd5b70a
Roberto Berrocal <r.berrocal@pucp.edu.pe>
2025-02-14 00:19:37 +0100

    feat: add profile and experience

--- rob-portal/src/components/Experience.tsx
+++ rob-portal/src/components/Experience.tsx
@@ -1,38 +1,82 @@
-import { Box, Heading, Text, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Heading,
+  SimpleGrid,
+  Card,
+  CardBody,
+  CardTitle,
+  CardDescription,
+  CardFooter,
+} from "@chakra-ui/react";
 
 const experiences = [
   {
-    role: "Data Analyst",
-    company: "Anheuser-Busch Inbev",
-    year: "2022 - 2024",
-  },
-  {
     role: "Software Engineer",
     company: "Affinidi GmbH",
-    year: "2024 - Presente",
+    year: "Sep 2024 - Present",
+  },
+  {
+    role: "Data Analyst",
+    company: "Anheuser-Busch Inbev",
+    year: "May 2022 - May 2024",
+  },
+  {
+    role: "Digital Solutions Professional Intern",
+    company: "Statkraft Perú S.A.",
+    year: "April 2021 - April 2022",
+  },
+  {
+    role: "IT Architecture Pre-Professional Intern",
+    company: "Banco de Crédito del Perú BCP",
+    year: "Jan 2020 - Dec 2020",
   },
 ];
 
 const Experience = () => {
   return (
-    <Box id="experience" py={10} px={8} minH={"100vh"} bg={"blue.800"}>
+    <Box
+      // width={"auto"}
+      id="experience"
+      py={10}
+      px={8}
+      minH={"100vh"}
+      bg={"blue.800"}
+      color={"white"}
+    >
       <Heading textAlign="center" mb={6}>
-        Experiencia
+        Work Experience
       </Heading>
-      <VStack
-      //   spacing={4}
-      >
+      <SimpleGrid columns={[4, 2]} px={[4, 10]} gap={4}>
         {experiences.map((exp, index) => (
-          <Box key={index} textAlign="center">
-            <Text fontSize="xl" fontWeight="bold">
-              {exp.role}
-            </Text>
-            <Text fontSize="md">
-              {exp.company} - {exp.year}
-            </Text>
-          </Box>
+          <Card.Root
+            _hover={{ bg: "blue.300" }}
+            textAlign={"center"}
+            key={index}
+            bg="blue.100"
+            color="black"
+            p={4}
+          >
+            <CardBody>
+              <CardTitle>{exp.role}</CardTitle>
+              <CardDescription
+                fontStyle={"italic"}
+                marginTop={2}
+                marginBottom={2}
+                fontWeight={"bold"}
+                color={"black"}
+              >
+                {exp.company}
+              </CardDescription>
+              {/* <Text fontSize="sm" fontStyle="italic">
+                {exp.year}
+              </Text> */}
+              <CardFooter justifyContent={"center"} padding={0}>
+                {exp.year}
+              </CardFooter>
+            </CardBody>
+          </Card.Root>
         ))}
-      </VStack>
+      </SimpleGrid>
     </Box>
   );
 };
