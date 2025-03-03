adc577bf1e697cce805358644add69c40765aef8
Roberto Berrocal <r.berrocal@pucp.edu.pe>
2025-03-04 00:18:23 +0100

    fix: fixes on card component

--- src/components/horizontalCard.tsx
+++ src/components/horizontalCard.tsx
@@ -1,111 +1,3 @@
-// import { Box, Image, Link, Text } from "@chakra-ui/react";
-// import { Card } from "@chakra-ui/react";
-// import { List } from "@chakra-ui/react";
-
-// interface HorizontalCardProps {
-//   imagePath?: string;
-//   title: string;
-//   subtitle?: string;
-//   description: string | string[];
-//   footer?: string;
-//   imgmaxW?: string;
-//   imgW?: string;
-//   maxW?:
-//     | "xs"
-//     | "sm"
-//     | "md"
-//     | "lg"
-//     | "xl"
-//     | "5xl"
-//     | "6xl"
-//     | "7xl"
-//     | "8xl"
-//     | "9xl";
-//   url_link?: string;
-// }
-
-// const HorizontalCard: React.FC<HorizontalCardProps> = ({
-//   title,
-//   subtitle,
-//   imagePath,
-//   description,
-//   footer,
-//   maxW = "xl",
-//   imgmaxW = "200px",
-//   imgW = "200px",
-//   url_link,
-// }) => {
-//   return (
-//     <Card.Root
-//       flexDirection="row"
-//       overflow="hidden"
-//       maxW={maxW}
-//       bg="white"
-//       _hover={{ bg: "gray.200" }}
-//       textAlign={"center"}
-//       color="black"
-//       p={4}
-//       m={5}
-//       shadow={"md"}
-//       borderRadius={"2xl"}
-//     >
-//       {/* Imagen */}
-//       <Box display="flex" justifyContent="center" alignItems="center">
-//         <Link href={url_link} target="_blank">
-//           <Image
-//             objectFit="cover"
-//             maxW={imgmaxW}
-//             src={imagePath}
-//             alt={title}
-//             w={imgW}
-//           />
-//         </Link>
-//       </Box>
-
-//       {/* Contenido */}
-//       <Box flex="1" textAlign="left">
-//         <Card.Body
-//           paddingTop={0}
-//           paddingBottom={0}
-//           paddingRight={4}
-//           marginLeft={4}
-//         >
-//           <Card.Title mb="2">
-//             {title}
-//             {subtitle && (
-//               <Text fontSize="sm" fontStyle="italic">
-//                 @{subtitle}
-//               </Text>
-//             )}
-//           </Card.Title>
-//           <Card.Description color="black">
-//             {Array.isArray(description) ? (
-//               <List.Root>
-//                 {description.map((desc, index) => (
-//                   <List.Item key={index}>{desc}</List.Item>
-//                 ))}
-//               </List.Root>
-//             ) : (
-//               <Text>{description}</Text>
-//             )}
-//           </Card.Description>
-//         </Card.Body>
-//       </Box>
-//       {/* Footer */}
-//       <Box display="flex" justifyContent="center" alignItems="center">
-//         {footer && (
-//           <Card.Footer>
-//             <Text fontSize="sm" fontStyle="italic">
-//               {footer}
-//             </Text>
-//           </Card.Footer>
-//         )}
-//       </Box>
-//     </Card.Root>
-//   );
-// };
-
-// export default HorizontalCard;
 import { Box, Image, Link, Text } from "@chakra-ui/react";
 import { Card } from "@chakra-ui/react";
 import { List } from "@chakra-ui/react";
@@ -118,7 +10,9 @@ interface HorizontalCardProps {
   footer?: string;
   imgmaxW?: string;
   imgW?: string;
+  imgH?: string;
   url_link?: string;
+  cardHeight?: string;
   maxW?:
     | "xs"
     | "sm"
@@ -141,7 +35,9 @@ const HorizontalCard: React.FC<HorizontalCardProps> = ({
   maxW = "xl",
   imgmaxW = "200px",
   imgW = "200px",
+  imgH,
   url_link,
+  cardHeight,
 }) => {
   return (
     <Card.Root
@@ -157,8 +53,9 @@ const HorizontalCard: React.FC<HorizontalCardProps> = ({
       m={5}
       shadow={"md"}
       borderRadius={"2xl"}
+      height={cardHeight}
     >
-      {/* Imagen */}
+      {/* Image */}
       <Box alignItems={"center"} alignContent={"center"}>
         {" "}
         <Link href={url_link} target="_blank">
@@ -169,11 +66,12 @@ const HorizontalCard: React.FC<HorizontalCardProps> = ({
             src={imagePath}
             alt={title}
             w={imgW}
+            h={imgH}
           />
         </Link>
       </Box>
 
-      {/* Contenido */}
+      {/* Content */}
       <Box flex="1" textAlign="left">
         <Card.Body
           paddingTop={0}
@@ -206,13 +104,11 @@ const HorizontalCard: React.FC<HorizontalCardProps> = ({
       {/* Footer */}
       {footer && (
         <Box alignContent={"center"} alignItems={"center"}>
-          {/* {footer && ( */}
           <Card.Footer paddingLeft={0} paddingRight={0} marginRight={4}>
             <Text fontSize="sm" fontStyle="italic">
               {footer}
             </Text>
           </Card.Footer>
-          {/* )} */}
         </Box>
       )}
     </Card.Root>
